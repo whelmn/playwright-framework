40feb08c6665e25b8595022802baeeb8ddf2226a
Will Helmn <willhelmn@gmail.com>
2026-03-10 13:37:49 +1000

    added users.json to get rid of hard coded values

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,19 +1,28 @@
 from playwright.sync_api import Page, Playwright
 import pytest
 from pages.login_page import LoginPage
+import json
+from pathlib import Path
+
 
 
 @pytest.fixture
-def login_page(page: Page):
-    page.goto("https://www.saucedemo.com/")
+def login_page(page: Page, grab_json_file):
+    page.goto(grab_json_file["urls"]["sauce_login"])
     yield page
 
 @pytest.fixture
-def logged_in_page(login_page):
+def logged_in_page(login_page, grab_json_file):
     login = LoginPage(login_page)
-    login.login("standard_user", "secret_sauce")
+    login.login(grab_json_file["users"]["standard_user"]["username"], grab_json_file["users"]["standard_user"]["password"])
     yield login_page
 
 @pytest.fixture(scope="session", autouse=True)
 def sauce_test_id(playwright: Playwright):
     playwright.selectors.set_test_id_attribute("data-test")
+
+@pytest.fixture(scope="session")
+def grab_json_file():
+    path = Path(__file__).parent.parent.parent / "test-data" / "users.json"
+    with path.open() as f:
+        return json.load(f)
--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import expect
 from pages.login_page import LoginPage
 
-def test_login(login_page):
+def test_login(login_page, grab_json_file):
     login = LoginPage(login_page)
-    login.login("standard_user", "secret_sauce")
-    expect(login_page).to_have_url("https://www.saucedemo.com/inventory.html")
+    login.login(grab_json_file["users"]["standard_user"]["username"], grab_json_file["users"]["standard_user"]["password"])
+    expect(login_page).to_have_url(grab_json_file["urls"]["sauce_inventory"])
